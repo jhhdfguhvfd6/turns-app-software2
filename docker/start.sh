a4610ea5eaedd0a93f067f9e204375a35ed5745f
jhhdfguhvfd6 <jrada7674@gmail.com>
2026-05-26 22:15:47 -0500

    fix: limpiar caches antes de regenerarlos al inicio

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
