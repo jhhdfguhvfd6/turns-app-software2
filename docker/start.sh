ae49479b9eb0b23b6c439ede62d875604a68c829
jhhdfguhvfd6 <jrada7674@gmail.com>
2026-05-26 22:25:53 -0500

    fix: remover route:cache del startup para evitar fallo por MongoDB

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
