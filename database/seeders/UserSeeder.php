eebc9d99155c2b7a1b94468d5e1adf20654dfb2c
jhhdfguhvfd6 <jrada7674@gmail.com>
2026-06-03 01:37:26 -0500

    Actualizar emails de usuarios de prueba

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,8 +17,8 @@ class UserSeeder extends Seeder
         User::truncate(); // Truncate funciona bien con MongoDB
         // Schema::enableForeignKeyConstraints();
 
-        User::create(['name' => 'Cliente Prueba','document_number' => '123456789','email' => 'cliente@prueba.com','password' => Hash::make('password'),'role' => 'client',]);
-        User::create(['name' => 'Cajero Prueba','document_number' => '987654321','email' => 'cajero@prueba.com','password' => Hash::make('password'),'role' => 'cashier',]);
-        User::create(['name' => 'Admin Prueba','document_number' => '111222333','email' => 'admin@prueba.com','password' => Hash::make('password'),'role' => 'admin',]);
+        User::create(['name' => 'Cliente Prueba','document_number' => '123456789','email' => 'jrada7674@gmail.com','password' => Hash::make('password'),'role' => 'client',]);
+        User::create(['name' => 'Cajero Prueba','document_number' => '987654321','email' => 'jrada7674@gmail.com','password' => Hash::make('password'),'role' => 'cashier',]);
+        User::create(['name' => 'Admin Prueba','document_number' => '111222333','email' => 'jrada7674@gmail.com','password' => Hash::make('password'),'role' => 'admin',]);
     }
 }
